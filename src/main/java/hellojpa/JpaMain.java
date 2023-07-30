6c492e6fc16dad137eefc67757ddaa28e958e651
laveberry <laveberry7@gmail.com>
2023-07-30 11:53:20 +0900

    플러시

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,14 +19,14 @@ public class JpaMain {
             //실제 코드 작성 부분
 
             //비영속
-            Member member = new Member();
-            member.setId(100L); //L : long타입 표시
-            member.setName("HelloJPA");
-
-            System.out.println("=== before === ");
-            //영속
-            em.persist(member);
-            System.out.println("=== after ===");
+//            Member member = new Member();
+//            member.setId(100L); //L : long타입 표시
+//            member.setName("HelloJPA");
+//
+//            System.out.println("=== before === ");
+//            //영속
+//            em.persist(member);
+//            System.out.println("=== after ===");
 
             //준영속 : 회원 엔티티를 영속성 컨텍스트에서 분리
 //            em.detach(member);
@@ -54,33 +54,47 @@ public class JpaMain {
 
             //1차 캐시 START==============================
             //조회 후 1차 캐시 저장
-            Member findMember = em.find(Member.class, 100L);
-            //조회 시 1차 캐시 조회(쿼리 실행안됨)
-            Member findMember2 = em.find(Member.class, 100L);
-
-            System.out.println("find id => "+ findMember2.getId());
-            System.out.println("find name => " + findMember2.getName());
-            //영속 엔티티 동일성 보장됨
-            System.out.println("result => " + (findMember == findMember2)); //true
+//            Member findMember = em.find(Member.class, 100L);
+//            //조회 시 1차 캐시 조회(쿼리 실행안됨)
+//            Member findMember2 = em.find(Member.class, 100L);
+//
+//            System.out.println("find id => "+ findMember2.getId());
+//            System.out.println("find name => " + findMember2.getName());
+//            //영속 엔티티 동일성 보장됨
+//            System.out.println("result => " + (findMember == findMember2)); //true
             //1차 캐시 END==============================
 
 
             //영속성 컨텍스트, 커밋 시점 확인 START=================================
             //영속성 컨텍스트에 저장
-            Member member1 = new Member(150L, "A");
-            Member member2 = new Member(160L, "B");
-
-            em.persist(member1);
-            em.persist(member2);
-            System.out.println("==========인서트쿼리 실행시점 확인===========");
+//            Member member1 = new Member(150L, "A");
+//            Member member2 = new Member(160L, "B");
 //
-            em.find(Member.class, 150L);
-            //자동 변경 감지 기능(자바 컬렉션 처럼)
-            findMember.setName("ZZZZ");
-            System.out.println("==========업데이트 쿼리 실행시점 확인===========");
-//            //이순간 db에 insert sql 작동됨
-            tx.commit();
+//            em.persist(member1);
+//            em.persist(member2);
+//            System.out.println("==========인서트쿼리 실행시점 확인===========");
+////
+//            em.find(Member.class, 150L);
+//            //자동 변경 감지 기능(자바 컬렉션 처럼)
+//            findMember.setName("ZZZZ");
+//            System.out.println("==========업데이트 쿼리 실행시점 확인===========");
             //영속성 컨텍스트, 커밋 시점 확인 END=================================
+
+
+            Member member3 = new Member(200L, "member200");
+            em.persist(member3);
+            /*
+            플러시
+            - 영속성 컨텍스트 비우지 않음
+            - 영속성 컨텍스트 변경내용을 db에 동기화
+           //커밋 반영전에 미리 실행하고 싶을때 플러시 사용
+            * */
+            em.flush();
+            System.out.println("==========플러시 시점 확인===========");
+
+            //이순간 db에 insert sql 작동됨
+            //플러시 자동 호출
+            tx.commit();
 
         }catch (Exception e){
             tx.rollback();
